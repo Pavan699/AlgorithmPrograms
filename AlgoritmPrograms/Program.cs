d4a7562c2c960469a20f6885b81355043a579b85
Pavan699 <pavannakate03@gmail.com>
2021-09-27 09:55:46 -0700

    Adding Check method to get the BLabz massege

--- AlgoritmPrograms/Program.cs
+++ AlgoritmPrograms/Program.cs
@@ -7,14 +7,38 @@ namespace AlgoritmPrograms
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome To Algorithmic Programs");
-
-            /* BinarySearch.Search();
-
-             InsertionSort.Insert();
-
-             BubbleSort.Bubble();*/
-
-            PrimeNumbers.Prime();
+            Console.WriteLine("1.BinarySearch\n2.InsertionSort\n3.BubbleSort\n4.PrimeNumbers\n5.Anagram\n6.CustomizeMessage\n7.Exit");
+            Console.Write("Enter the Choice : ");
+            int choice = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine();
+            switch (choice)
+            {
+                case 1:
+                    BinarySearch.Search();
+                    break;
+                case 2:
+                    InsertionSort.Insert();
+                    break;
+                case 3:
+                    BubbleSort.Bubble();
+                    break;
+                case 4:
+                    PrimeNumbers.Prime();
+                    break;
+                case 5:
+                    Anagram ana = new Anagram();
+                    String str1 = "abcd";
+                    int num = str1.Length;
+                    ana.anagram(str1, 0, num - 1);
+                    break;
+                case 6:
+                    RegexInput regexp = new RegexInput();
+                    regexp.Check();
+                    break;
+                default:
+                    Console.WriteLine("Thank You... :)");
+                    break;
+            }
         }
     }
 }
